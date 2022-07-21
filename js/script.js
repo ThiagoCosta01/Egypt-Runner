511e086bce36afe87790b6b8a284b51733e62dc5
Thiago <thiago10lscbf@gmail.com>
2022-07-21 02:18:56 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const girl = document.querySelector(".girl")
 const bat = document.querySelector(".bat")
+const cloud = document.querySelector(".clouds") 
+const canvas = document.querySelector(".container")
+const centro = document.querySelector(".centro")
+
 
 const jump = () => {
     girl.classList.add("jump")
@@ -15,18 +19,33 @@ const loop = setInterval(() => {
 
     const batPosition = bat.offsetLeft;
     const girlPosition = +window.getComputedStyle(girl).bottom.replace("px","");
+    const cloudsPosition = +window.getComputedStyle(cloud).right.replace("px","");
 
-    console.log(batPosition)
     if (batPosition <= 120 && batPosition > 0 && girlPosition < 95){
         bat.style.animation = "none";
         bat.style.left = `${batPosition}px`;
         girl.style.bottom = `${girlPosition}px`;
         girl.style.animation = "none"; 
 
+        girl.src="./img/boyLosest.png";
+        girl.style.bottom = `${41}px`;
 
+        cloud.style.animation = "none"
+        cloud.style.right = `${cloudsPosition}px`;
+
+        centro.innerText = "Aperte Ctrl + R para continuar";
+        
+
+        clearInterval(loop)
     }
 
 }, 20)
+
+const upSpeed = setInterval(()=>{
+
+
+
+}, 5000)
 
 document.addEventListener("keydown", jump)
 
